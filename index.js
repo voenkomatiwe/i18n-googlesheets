bfe6b258a46b3889251c9a8b7864b0e8b17e8441
voenkomatiwe <radionpon21@gmail.com>
2023-10-30 15:24:44 +0200

    fixed: the logic for generateFilesFromSpreadsheet has been reworked

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-const path = require('path');
-const process = require('process');
+const { env } = require('process');
 
-const { generateFilesFromSpreadsheet } = require('../lib/generateFilesFromSpreadsheet');
+const { generateFilesFromSpreadsheet } = require('./lib/generateFilesFromSpreadsheet');
 
-const SPREAD_SHEET_ID = '1u2eumiwPFT4EevSiHDQCq83xDVkDyjehz5IJ0IfooJA'
-const DEFAULT_CREDENTIALS = path.join(process.cwd(), 'credentials.json')
-const DEFAULT_TOKEN = path.join(process.cwd(), 'token.json')
-const DEFAULT_RANGE = 'Sheet1'
-const DEFAULT_KEY_INDEX = 0
-const DEFAULT_LANG_INDEX = 1
-const DEFAULT_OUTPUT_DIR = './locales'
-const DEFAULT_FORMAT = 'json'
-const DEFAULT_BEAUTIFY = 4
+const SPREAD_SHEET_ID = '1tzuc3vxLVPo8wSOvOOSDwsH7yMcQSZeE9XULntzAlag';
+const DEFAULT_API_KEY = env.API_KEY;
+const DEFAULT_OUTPUT_DIR = './locales';
+const DEFAULT_FORMAT = 'json';
+const DEFAULT_BEAUTIFY = 2;
 
 generateFilesFromSpreadsheet(
 	SPREAD_SHEET_ID,
-	DEFAULT_CREDENTIALS,
-	DEFAULT_TOKEN,
-	DEFAULT_RANGE,
-	DEFAULT_KEY_INDEX,
-	DEFAULT_LANG_INDEX,
+  DEFAULT_API_KEY,
 	DEFAULT_OUTPUT_DIR,
 	DEFAULT_FORMAT,
 	DEFAULT_BEAUTIFY,
-)
+);
